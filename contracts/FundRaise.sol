83f1a849f5d9ec58a6df3341519be29747cac61b
MINJE-98 <jmj012100@gmail.com>
2021-09-16 22:14:08 +0900

    EDIT close #5 생성자, 구조체 작성

--- contracts/FundRaise.sol
+++ contracts/FundRaise.sol
@@ -3,10 +3,26 @@
 pragma solidity ^0.8.0;
 
 contract FundRaise {
+    podoInterface public podo;
+    Project public projects;
+
+    struct Project {
+        string title;
+        string desc;
+        uint256 targetmoney;
+        uint256 currentmoney;
+        uint256 state;
+        address[] donator;
+    }
     // 특정 지갑이 입금한 전체 이더를 저장
     mapping(address => uint256) totalBalance;
     // 특정 지갑이 입금한 이더에 PodoToken으로 Swap 가능한 개수
     mapping(address => uint256) payableBalance;
+
+    // Podo 토큰의 메서드를 호출하기 위해 생성자에서 Podo 토큰 주소를 입력합니다.
+    constructor(address _podo) {
+        podo = podoInterface(_podo);
+    }
 
     // 이더리움을 받는 메서드
     receive() external payable {
@@ -14,4 +30,8 @@ contract FundRaise {
         totalBalance[msg.sender] += msg.value;
         payableBalance[msg.sender] += msg.value;
     }
+}
+
+interface podoInterface {
+    function mint(address _to, uint256 _amount) external;
 }
