49c88999ad48e83664d225f469ca6d39fa11b013
MINJE-98 <jmj012100@gmail.com>
2021-09-18 15:23:19 +0900

    EDIT close #11 createProject 메서드 추가 및 몇가지 메서드 비활성화

--- contracts/FundRaise.sol
+++ contracts/FundRaise.sol
@@ -1,21 +1,46 @@
 // SPDX-License-Identifier: MIT
 
+import "./openzeppelin/SafeMath.sol";
+
 pragma solidity ^0.8.0;
 
 contract FundRaise {
-    podoInterface public podo;
-    Project public projects;
+    using SafeMath for uint256;
 
+    uint256 public mint;
+    uint256 public projectCount;
+    string public empty = "";
+
+    function projectDelay() public pure returns (uint256) {
+        return 1;
+    }
+
+    function projectPeriod() public pure returns (uint256) {
+        return 17280;
+    }
+
+    /**
+    프로젝트에 여러 기부자들이 있다.
+
+     */
     struct Project {
+        uint256 id;
         string title;
         string desc;
-        uint256 targetmoney;
-        uint256 currentmoney;
-        uint256 state;
-        address[] donator;
+        uint256 targetMoney;
+        uint256 currentMoney;
+        uint256 startBlock;
+        uint256 endBlock;
+        mapping(address => Donator) donators;
     }
-    // 특정 지갑이 입금한 전체 이더를 저장
-    mapping(address => uint256) totalBalance;
+    struct Donator {
+        address donatorAddress;
+        uint256 amount;
+    }
+    podoInterface public podo;
+    Project[] public project;
+    // 프로젝트 번호 별로 매핑
+    mapping(uint256 => Project) public projects;
     // 특정 지갑이 입금한 이더에 PodoToken으로 Swap 가능한 개수
     mapping(address => uint256) payableBalance;
 
@@ -25,22 +50,48 @@ contract FundRaise {
     }
 
     // 이더리움을 받는 메서드
-    receive() external payable {
-        // 이더리움을 입금 받고 받은 만큼 배열에 저장
-        totalBalance[msg.sender] += msg.value;
-        payableBalance[msg.sender] += msg.value;
-    }
+    // receive() external payable {
+    //     // 이더리움을 입금 받고 받은 만큼 배열에 저장
+    //     totalBalance[msg.sender] += msg.value;
+    //     payableBalance[msg.sender] += msg.value;
+    // }
 
     // 사용자가 입금한 전체 이더리움 출력합니다.
-    function total(address _to) public view returns (uint256) {
-        return totalBalance[_to];
-    }
+    // function total(address _to) public view returns (uint256) {
+    //     return totalBalance[_to];
+    // }
 
     // PodoToken으로 스왑합니다.
     function swapEtherToPodoToken(address _to, uint256 _amount) public {
         require(payableBalance[_to] >= 0);
         payableBalance[_to] -= _amount;
         podo.mint(_to, _amount);
+    }
+
+    function createProject(
+        string memory _title,
+        string memory _desc,
+        uint256 _targetmoney
+    ) public returns (uint256) {
+        require(
+            keccak256(bytes(_title)) != keccak256(bytes(empty)) &&
+                keccak256(bytes(_desc)) != keccak256(bytes(empty)) &&
+                _targetmoney != 0
+        );
+        // 시작 블럭, 종료 블럭으로 기간 지정
+        uint256 startBlock = block.number.add(projectDelay());
+        uint256 endBlock = startBlock.add(projectPeriod());
+
+        projectCount++;
+        Project storage newProject = projects[projectCount];
+        newProject.id = projectCount;
+        newProject.title = _title;
+        newProject.desc = _desc;
+        newProject.targetMoney = _targetmoney;
+        newProject.currentMoney = 0;
+        newProject.startBlock = startBlock;
+        newProject.endBlock = endBlock;
+        return newProject.id;
     }
 }
 
