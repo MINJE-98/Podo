82098be8d5e4eb61a1ab81ef21ba7d744d92b524
MINJE-98 <jmj012100@gmail.com>
2021-09-23 11:42:05 +0900

    EDIT 유저정보 관련 Ballot로 분리

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -9,3 +9,24 @@ contract BALLOT is ERC20("ballot", "BALLOT") {
         _mint(_to, _amount);
     }
 }
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+import "./openzeppelin/IERC20.sol";
+import "./openzeppelin/ERC20.sol";
+import "./openzeppelin/SafeMath.sol";
+
+contract Vote is ERC20("Vote", "VOTE") {
+    using SafeMath for uint256;
+    // 유저정보
+    struct UserInfo {
+        uint256 amount;
+    }
+    // 그룹 주소 -> 프로젝트 pid -> 유저 주소
+    mapping(address => mapping(uint256 => mapping(address => UserInfo)))
+        public userInfo;
+
+    function mint(address _to, uint256 _amount) external {
+        _mint(_to, _amount);
+    }
+}
