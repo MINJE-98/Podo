0fc7165018b159d49f6f5a037f187ac42146baeb
MINJE-98 <jmj012100@gmail.com>
2021-09-28 15:57:07 +0900

    ADD close #41 Governor 구조 잡기

--- contracts/Governor.sol
+++ contracts/Governor.sol
@@ -2,7 +2,14 @@
 
 pragma solidity ^0.8.0;
 
+import "./openzeppelin/IERC20.sol";
+
 contract Governor {
+    bytes32 public empty = keccak256(bytes(""));
+    IERC20 public podo;
+    IERC20 public ballot;
+    FunRaiseInterface public fundRaise;
+
     // 제안 정보
     struct Proposal {
         string title;
@@ -29,6 +36,17 @@ contract Governor {
     // 그룹 주소 -> 프로젝트 pid -> 제안
     mapping(address => mapping(uint256 => Proposal)) proposal;
 
+    //  컨태랙트 주입
+    constructor(
+        IERC20 _podo,
+        IERC20 _ballot,
+        address _fundRaise
+    ) {
+        podo = _podo;
+        ballot = _ballot;
+        fundRaise = FunRaiseInterface(_fundRaise);
+    }
+
     /**
         제안 생성
      */
@@ -53,4 +71,8 @@ contract Governor {
         부결
      */
     function castVote() public {}
+}
+
+interface FunRaiseInterface {
+    function hasGorup() external;
 }
