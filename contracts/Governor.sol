4aa8a1bd2a072e2ebef9f3fd2a8a8efc2daf5ee2
MINJE-98 <jmj012100@gmail.com>
2021-09-28 14:53:50 +0900

    ADD close #41 Governor 구조 잡기

--- contracts/Governor.sol
+++ contracts/Governor.sol
@@ -2,4 +2,55 @@
 
 pragma solidity ^0.8.0;
 
-contract Governor {}
+contract Governor {
+    // 제안 정보
+    struct Proposal {
+        string title;
+        string desc;
+        uint256 forVotes;
+        uint256 againstVotes;
+        uint256 startBlock;
+        uint256 endBlock;
+        bool executed;
+        bool canceled;
+    }
+    // 제안 상태
+    enum ProposalState {
+        Pending,
+        Active,
+        Canceled,
+        Defeated,
+        Succeeded,
+        Queued,
+        Expired,
+        Executed
+    }
+
+    // 그룹 주소 -> 프로젝트 pid -> 제안
+    mapping(address => mapping(uint256 => Proposal)) proposal;
+
+    /**
+        제안 생성
+     */
+    function propose() public {}
+
+    /**
+        제안 상태 보기
+     */
+    function state() public {}
+
+    /**
+        통과
+     */
+    function execute() public {}
+
+    /**
+        부결
+     */
+    function cancel() public {}
+
+    /**
+        부결
+     */
+    function castVote() public {}
+}
