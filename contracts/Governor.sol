4fe4b89675c5e4e50c49dd4502626a5b826f33fa
MINJE-98 <jmj012100@gmail.com>
2021-09-29 14:31:44 +0900

    EDIT close #47 TimeLock 인터페이스 추가

--- contracts/Governor.sol
+++ contracts/Governor.sol
@@ -178,6 +178,40 @@ contract Governor {
     function castVote() public {}
 }
 
+interface TimelockInterface {
+    function delay() external view returns (uint256);
+
+    function GRACE_PERIOD() external view returns (uint256);
+
+    function acceptAdmin() external;
+
+    function queuedTransactions(bytes32 hash) external view returns (bool);
+
+    function queueTransaction(
+        address target,
+        uint256 value,
+        string calldata signature,
+        bytes calldata data,
+        uint256 eta
+    ) external returns (bytes32);
+
+    function cancelTransaction(
+        address target,
+        uint256 value,
+        string calldata signature,
+        bytes calldata data,
+        uint256 eta
+    ) external;
+
+    function executeTransaction(
+        address target,
+        uint256 value,
+        string calldata signature,
+        bytes calldata data,
+        uint256 eta
+    ) external payable returns (bytes memory);
+}
+
 interface FunRaiseInterface {
     function viewGroupProjectInfo(address _group, uint256 _pid) external;
 }
