50e844d6b94148cc69081293b99c516c307a3b89
MINJE-98 <jmj012100@gmail.com>
2021-10-03 20:09:04 +0900

    EIDT TimeLock 제거 배포자 지정

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,14 +1,12 @@
 var FundRaise = artifacts.require("./FundRaise");
 var Ballot = artifacts.require("./Ballot");
 var Podo = artifacts.require("./Podo");
-var TimeLock = artifacts.require("./TimeLock");
 var Governor = artifacts.require("./Governor");
 
 module.exports = async function(deployer) {
   const admin = "0x51AA32D176b0dF3D1Af9E3Cfc9605977bFC52547"
-  await deployer.deploy(Podo);
-  await deployer.deploy(Ballot);
-  await deployer.deploy(TimeLock, admin, 177200);
-  await deployer.deploy(FundRaise, Podo.address, Ballot.address);
-  await deployer.deploy(Governor, Podo.address, Ballot.address, FundRaise.address,  TimeLock.address);
+  await deployer.deploy(Podo, {from: admin});
+  await deployer.deploy(Ballot, {from: admin});
+  await deployer.deploy(FundRaise, Podo.address, Ballot.address, {from: admin});
+  await deployer.deploy(Governor, Podo.address, Ballot.address, FundRaise.address);
 };
