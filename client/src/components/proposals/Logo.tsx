d7ce7a839821efe8455aee0e295534fdd9e8ff9a
MINJE-98 <jmj012100@gmail.com>
2022-02-19 14:00:58 +0900

    fix: 메인주소 변경

--- client/src/components/proposals/Logo.tsx
+++ client/src/components/proposals/Logo.tsx
@@ -1,19 +1,14 @@
-import { Flex, Image } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
-import logo from "../../res/PODO.png"
+import { Flex, Image } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import logo from '../../res/PODO.png';
 
-export default function Logo({child}:any) {
+export default function Logo({ child }: any) {
   return (
-  <Link to="/podoo">
-    <Flex
-      flex="1"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Image src={logo}  height="20px"width="20px"/>
-      {child}
-    </Flex>
-  </Link>
-  )
-
+    <Link to='/podo'>
+      <Flex flex='1' justifyContent='center' alignItems='center'>
+        <Image src={logo} height='20px' width='20px' />
+        {child}
+      </Flex>
+    </Link>
+  );
 }
