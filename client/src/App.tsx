5aa8f821e79dad8d4290bb0ad4fbe2e42fac5e0a
MINJE-98 <jmj012100@gmail.com>
2022-02-19 14:00:50 +0900

    fix: 메인주소 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,43 +1,43 @@
-import { ChakraProvider, useDisclosure } from "@chakra-ui/react";
+import { ChakraProvider, useDisclosure } from '@chakra-ui/react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import theme from "./theme";
-import Layout from "./components/Layout";
-import "@fontsource/inter";
-import Headers from "./containers/header/Headers";
-import Home from "./containers/home/Home";
-import Groups from "./containers/groups/Groups";
-import Campaigns from "./containers/campaigns/Campaigns";
-import Proposals from "./containers/proposals/Proposals";
-import MyPage from "./containers/mypage/MyPage";
-import GroupCreate from "./containers/groups/GroupCreate";
-import GroupInfo from "./containers/groups/GroupInfo";
-import CampaignCreate from "./containers/campaigns/CampaignCreate";
-import CampaignInfo from "./containers/campaigns/CampaignInfo";
-import ProposalCreate from "./containers/proposals/ProposalCreate";
-import ProposalInfo from "./containers/proposals/ProposalInfo";
+import theme from './theme';
+import Layout from './components/Layout';
+import '@fontsource/inter';
+import Headers from './containers/header/Headers';
+import Home from './containers/home/Home';
+import Groups from './containers/groups/Groups';
+import Campaigns from './containers/campaigns/Campaigns';
+import Proposals from './containers/proposals/Proposals';
+import MyPage from './containers/mypage/MyPage';
+import GroupCreate from './containers/groups/GroupCreate';
+import GroupInfo from './containers/groups/GroupInfo';
+import CampaignCreate from './containers/campaigns/CampaignCreate';
+import CampaignInfo from './containers/campaigns/CampaignInfo';
+import ProposalCreate from './containers/proposals/ProposalCreate';
+import ProposalInfo from './containers/proposals/ProposalInfo';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Layout>
-      <Router>
+        <Router>
           <Headers />
           <Switch>
-            <Route exact path='/podoo' component={Home} />
-            <Route path='/groups'component={Groups} />
-            <Route path='/groupInfo'component={GroupInfo} />
-            <Route path='/campaigns'component={Campaigns} />
-            <Route path='/campaignInfo'component={CampaignInfo} />
-            <Route path='/proposals'component={Proposals} />
-            <Route path='/proposalInfo'component={ProposalInfo} />
+            <Route exact path='/podo' component={Home} />
+            <Route path='/groups' component={Groups} />
+            <Route path='/groupInfo' component={GroupInfo} />
+            <Route path='/campaigns' component={Campaigns} />
+            <Route path='/campaignInfo' component={CampaignInfo} />
+            <Route path='/proposals' component={Proposals} />
+            <Route path='/proposalInfo' component={ProposalInfo} />
             <Route path='/mypage' component={MyPage} />
             <Route path='/groupcreate' component={GroupCreate} />
             <Route path='/campaigncreate' component={CampaignCreate} />
             <Route path='/proposalcreate' component={ProposalCreate} />
-            </Switch>
-      </Router>
+          </Switch>
+        </Router>
       </Layout>
-        {/* <AccountModal isOpen={isOpen} onClose={onClose} />
+      {/* <AccountModal isOpen={isOpen} onClose={onClose} />
         <Count /> */}
     </ChakraProvider>
   );
--- client/src/components/header/Logo.tsx
+++ client/src/components/header/Logo.tsx
@@ -1,27 +1,28 @@
-import { Flex, Image, Text, useBreakpointValue, useColorModeValue } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
-import logo from "../../res/PODO.png"
+import {
+  Flex,
+  Image,
+  Text,
+  useBreakpointValue,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import logo from '../../res/PODO.png';
 
 export default function Logo() {
   return (
-  <Link to="/podoo">
-    <Flex
-      flex="1"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Image src={logo}  height="2rem" width="2rem"/>
-      <Text
-        mx="3"
-        fontSize="xl"
-        fontWeight="bold"
-        color="purple.500"
-        textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
-        fontFamily={'heading'}>
-        PODO
+    <Link to='/podo'>
+      <Flex flex='1' justifyContent='center' alignItems='center'>
+        <Image src={logo} height='2rem' width='2rem' />
+        <Text
+          mx='3'
+          fontSize='xl'
+          fontWeight='bold'
+          color='purple.500'
+          textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
+          fontFamily={'heading'}>
+          PODO
         </Text>
-    </Flex>
-  </Link>
-  )
-
+      </Flex>
+    </Link>
+  );
 }
